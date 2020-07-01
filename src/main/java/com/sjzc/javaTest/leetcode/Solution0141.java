7af63d4da88a8dd19adfa35c933c808d4f1c0209
赵崇 <sjzc66@163.com>
2020-07-01 22:45:50 +0800

    leetcode 141

---
+++ src/main/java/com/sjzc/javaTest/leetcode/Solution0141.java
@@ -0,0 +1,64 @@
+package com.sjzc.javaTest.leetcode;
+
+/**
+ * @author zhaochong
+ * @Description 链表是否存在环
+ * <p>
+ * Solution0509
+ * @create: 2020-04-26 20:13
+ **/
+public class Solution0141 {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        // l5.next = l2;
+        // l1.print();
+
+        Solution0141 solution0021 = new Solution0141();
+        System.out.println(solution0021.hasCycle(l1));
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        void print() {
+            ListNode temp = this;
+            while (temp.next != null) {
+                System.out.print(temp.val + "->");
+                temp = temp.next;
+            }
+            System.out.println(temp.val);
+        }
+    }
+}
+
+
